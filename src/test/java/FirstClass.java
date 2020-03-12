fc9c7a591bcbc16405c11adebf302bac0e35e28e
Muhammad <musadam28@gmail.com>
2020-03-11 20:23:24 -0400

    third commit

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,6 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("brave new world");
-        System.out.println("hello world");
+        System.out.println("hi");
+        System.out.println("hey");
     }
 }
