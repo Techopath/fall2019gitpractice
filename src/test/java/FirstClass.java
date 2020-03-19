a1b105a67e5b6e3c4187f4054adc5a381119381b
Techopath <58576711+Techopath@users.noreply.github.com>
2020-03-18 20:57:23 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,16 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("brave new world");
-         System.out.println("brave new world");
-         System.out.println("brave new world");
-       
-        System.out.println("someone changed the code.");
-
-        System.out.println("ne change");
-        
-        
-        System.out.println("two change");
-        
-        System.out.println("4 change");
-    }
+        System.out.println("file changed now");
+         
 }
