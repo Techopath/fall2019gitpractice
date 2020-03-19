c5936812595fa66724411e5b022e9738eda9a43e
Techopath <58576711+Techopath@users.noreply.github.com>
2020-03-18 20:13:10 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,6 +2,8 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("brave new world");
+         System.out.println("brave new world");
+         System.out.println("brave new world");
        
         System.out.println("someone changed the code.");
     }
