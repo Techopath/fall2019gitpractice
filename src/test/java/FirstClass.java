7fbc7b3325314a1dd2021e025bb96481bed6aed0
Muhammad <musadam28@gmail.com>
2020-03-25 20:21:42 -0400

    after making changes in IJ and github

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,8 +1,13 @@
 public class FirstClass {
     public static void main(String[] args) {
 
+//<<<<<<< Updated upstream
         System.out.println("brave new world");
         System.out.println("file changed now"); //comment in github
+//=======
+        System.out.println("brave new world"); //comment in IntelliJ
+        System.out.println("file changed now");
+//>>>>>>> Stashed changes
         System.out.println("1 changed file");
         
         System.out.println("2 changed files"); //changed in github
@@ -10,6 +15,10 @@ public class FirstClass {
         System.out.println("3 files changed"); //changed in intelliJ
         
         System.out.println("4 changed files"); //changed in github
+
+
+        System.out.println("5 files changed"); //changed in intelliJ
+
 
     }
 
